ffd6ea8d5021d04be7c930fc810067feeb26e493
TomiV <tomi.valtanen1991@gmail.com>
2023-10-31 18:02:48 +0200

    test test

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Ilia</Text>
+      <Text>Tomi Valtanen</Text>
       <StatusBar style="auto" />
     </View>
   );
