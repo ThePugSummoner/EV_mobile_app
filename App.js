2ab536dc76c66d0e5706f48be4c882ad1da64d4f
Jere Pihlajasaari <jere.pihlajasaari@hotmail.com>
2023-11-02 14:24:12 +0200

    Bottom tab nav created, some colors and icons

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Profile from './components/Profile';
+import Mainpage from './components/MainPage';
+import Chargingstations from './components/ChargingStations';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Ilia</Text>
-      <Text>Tomi Valtanen</Text>
-      <Text>Jenny Kari</Text>
-      <Text>Jere Pihlajasaari</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        //initialRouteName="Feed"
+        sceneContainerStyle={{backgroundColor: 'transparent'}}
+        screenOptions={({ route }) => ({ 
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Profile') {
+              iconName = focused
+              ? 'account'
+              : 'account-outline'
+            }
+            else if (route.name === 'Main Page') {
+              iconName = focused
+              ? 'car-sports'
+              : 'car-sports'
+            }
+            else if (route.name === 'Charging Stations') {
+              iconName = focused
+              ? 'lightning-bolt'
+              : 'lightning-bolt-outline'
+            }
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />
+          
+          },
+          tabBarStyle: { backgroundColor: '#1D1A39' },
+          tabBarActiveTintColor: 'khaki',
+          tabBarInactiveTintColor: 'lightyellow'
+        })}
+      >
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Main Page" component={Mainpage} />
+        <Tab.Screen name="Charging Stations" component={Chargingstations} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/ChargingStations.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+import styles from '../style/style';
+
+export default ChargingStation = ({ navigation }) => {
+    return (
+        <Text>Testi vaan</Text>
+    );
+}
