6367e8eb9841722bf6b5675d4057331a7b349b31
Jealbepi <jere.pihlajasaari@hotmail.com>
2023-12-13 17:33:12 +0200

    Modalin buttonin border radiusta pienemmäksi

--- style/style.js
+++ style/style.js
@@ -391,7 +391,7 @@ const MainPageStyle = StyleSheet.create({
     modalView: {
         margin: 20,
         backgroundColor: '#cbb26a',
-        borderRadius: 15,
+        borderRadius: 8,
         padding: 25,
         alignItems: 'center',
         shadowColor: '#000',
@@ -404,7 +404,7 @@ const MainPageStyle = StyleSheet.create({
         elevation: 10,
     },
     button: {
-        borderRadius: 15,
+        borderRadius: 8,
         padding: 10,
         elevation: 4,
         marginTop: 5
